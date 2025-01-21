009d447f09921806fff40a6b5ce0649696a5c864
KAZUKI Otsuhata <7222225+k4zuki02h4t4@users.noreply.github.com>
2025-01-21 11:04:20 +0900

    delete host permission.

--- background.js
+++ background.js
@@ -1,17 +1,12 @@
 chrome.action.onClicked.addListener((tab) => {
-  if (tab.url && tab.url.match(/^https:\/\/www\.youtube\.com\/watch\?v=[^&]+/)) {
+  if (tab.url.match(/^https:\/\/www\.youtube\.com\/watch\?v=[^&]+/)) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: runBookmarklet
     });
-  } else {
-    console.log("This extension only works on YouTube.");
   }
 });
 
 function runBookmarklet() {
-    const url=window.document.URL;
-    const videoMatch=url.match(/^https:\/\/www\.youtube\.com\/watch\?v=([^&]+)/);
-
-    if(videoMatch){window.location.href=url+'&list=ULcxqQ59vzyTk';}else{console.log("This extension only works on YouTube.")};
+  window.location.href=window.document.URL+'&list=ULcxqQ59vzyTk';
 }
